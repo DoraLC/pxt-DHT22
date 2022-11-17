ac1216a31c2b31aafedb09d41557df36811ba6bf
DoraLC <34760377+DoraLC@users.noreply.github.com>
2022-11-17 17:33:42 +0800

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -9,6 +9,10 @@ enum dht22type {
 
 //% color=#F6421B icon="\uf2c9" block="DHT22"
 namespace DHT22 {
+    let temp_C = -999
+    let temp_F = -999
+    let humid = -999
+
     let pin = DigitalPin.P0
     function signal_dht22(pin: DigitalPin): void {
         pins.digitalWritePin(pin, 0)
@@ -21,12 +25,11 @@ namespace DHT22 {
      * Set up the sensor and start reading data.
      *
      */
-    //% block="DHT22 pin %pin_arg|data %data_type"
-    //% blockId=reading_dht22_data
+    //% block="READ DHT22 pin %pin_arg"
     //% pin_arg.fieldEditor="gridpicker" pin_arg.fieldOptions.columns=5
     //% pin_arg.fieldOptions.tooltips="false"
     //% weight=0
-    export function dht22_read(pin_arg: DigitalPin, data_type: dht22type): number {
+    export function dht22_read(pin_arg: DigitalPin) {
         basic.pause(3000)
         pin = pin_arg
         signal_dht22(pin)
@@ -37,9 +40,9 @@ namespace DHT22 {
         while (pins.digitalReadPin(pin) == 1);
 
         let counter = 0
-        let value = 0
-        let value2 = 0
-        let value3 = 0
+        let humid_raw = 0
+        let temp_raw = 0
+        let check_sum = 0
 
         for (let i = 0; i <= 40 - 1; i++) {
             while (pins.digitalReadPin(pin) == 0);
@@ -49,26 +52,33 @@ namespace DHT22 {
             }
             if (counter > 2) {
                 if (i < 16) {
-                    value = value + (1 << (15 - i));
+                    humid_raw = humid_raw + (1 << (15 - i));
                 }
                 else if (i < 32) {
-                    value2 = value2 + (1 << (31 - i))
+                    temp_raw = temp_raw + (1 << (31 - i))
                 }
                 else {
-                    value3 = value3 + (1 << (39 - i))
+                    check_sum = check_sum + (1 << (39 - i))
                 }
             }
         }
 
+        temp_C = temp_raw / 10
+        temp_F = temp_C * 9 / 5 + 32
+        humid = humid_raw / 10
+    }
+
+    //% block="data %data_type"
+    export function data(data_type: dht22type): number {
         switch (data_type) {
             case 0:
-                return value2 / 10
+                return temp_C
             case 1:
-                return (value2 / 10) * 9 / 5 + 32
+                return temp_F
             case 2:
-                return value / 10
+                return humid
             default:
-                return 0
+                return -999
         }
     }
 }
