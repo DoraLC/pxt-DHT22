a3ba1202628d96543394b83e6b9adb432e65ec48
DoraLC <34760377+DoraLC@users.noreply.github.com>
2024-02-28 14:45:17 +0800

    update

--- main.ts
+++ main.ts
@@ -56,38 +56,53 @@ namespace DHT22 {
         pin = pin_arg
         signal_dht22(pin)
 
+        let responsed = true
+        let responseStartTime = control.millis()
+
         // Wait for response header to finish
-        while (pins.digitalReadPin(pin) == 1);
+        while (pins.digitalReadPin(pin) == 1) {
+            if (control.millis() - responseStartTime > 5000) {
+                responsed = false
+                break
+            }
+        };
         while (pins.digitalReadPin(pin) == 0);
-        while (pins.digitalReadPin(pin) == 1);
-
-        let counter = 0
-        let humid_raw = 0
-        let temp_raw = 0
-        let check_sum = 0
-
-        for (let i = 0; i <= 40 - 1; i++) {
-            while (pins.digitalReadPin(pin) == 0);
-            counter = 0
-            while (pins.digitalReadPin(pin) == 1) {
-                counter += 1;
+        while (pins.digitalReadPin(pin) == 1) {
+            if (control.millis() - responseStartTime > 5000) {
+                responsed = false
+                break
             }
-            if (counter > counter_limit) {
-                if (i < 16) {
-                    humid_raw = humid_raw + (1 << (15 - i));
+        };
+
+        if (responsed) {
+            let counter = 0
+            let humid_raw = 0
+            let temp_raw = 0
+            let check_sum = 0
+
+            for (let i = 0; i <= 40 - 1; i++) {
+                while (pins.digitalReadPin(pin) == 0);
+                counter = 0
+                while (pins.digitalReadPin(pin) == 1) {
+                    counter += 1;
                 }
-                else if (i < 32) {
-                    temp_raw = temp_raw + (1 << (31 - i))
-                }
-                else {
-                    check_sum = check_sum + (1 << (39 - i))
+                if (counter > counter_limit) {
+                    if (i < 16) {
+                        humid_raw = humid_raw + (1 << (15 - i));
+                    }
+                    else if (i < 32) {
+                        temp_raw = temp_raw + (1 << (31 - i))
+                    }
+                    else {
+                        check_sum = check_sum + (1 << (39 - i))
+                    }
                 }
             }
+
+            temp_C = temp_raw / 10
+            temp_F = temp_C * 9 / 5 + 32
+            humid = humid_raw / 10
         }
-
-        temp_C = temp_raw / 10
-        temp_F = temp_C * 9 / 5 + 32
-        humid = humid_raw / 10
     }
 
     //% block="data %data_type"
